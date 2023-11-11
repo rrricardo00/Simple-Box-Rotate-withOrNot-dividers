e845b9401e46fdd3fa34e991d9e9bccbe6446b98
rrricardo00 <rrricardo00@yahoo.com.br>
2023-11-11 16:06:45 -0300

    condition

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-var container = document.querySelector('.cube');
+let container = document.querySelector(".cube");
 
-container.addEventListener('mousemove', function(event) {
- var centerX = container.clientWidth / 2;
- var centerY = container.clientHeight / 2;
- var angleX = (event.clientX - centerX) / centerX * 90;
- var angleY = (event.clientY - centerY) / centerY * -90;
- var angleZ = (event.clientX - centerX) / centerX * -45;
- container.style.transform = 'rotateX(' + angleY + 'deg) rotateY(' + angleX + 'deg) rotateZ(' + angleZ + 'deg)';
-});
+if (container) {
+  container.addEventListener("mousemove", function (event) {
+    let centerX = container.clientWidth / 2;
+    let centerY = container.clientHeight / 2;
+    let angleX = ((event.clientX - centerX) / centerX) * 90;
+    let angleY = ((event.clientY - centerY) / centerY) * -90;
+    let angleZ = ((event.clientX - centerX) / centerX) * -45;
+    container.style.transform =
+      "rotateX(" +
+      angleY +
+      "deg) rotateY(" +
+      angleX +
+      "deg) rotateZ(" +
+      angleZ +
+      "deg)";
+  });
 
-container.addEventListener('mouseleave', function() {
-    container.style.transform = 'rotateX(' + angleY + 'deg) rotateY(' + angleX + 'deg) rotateZ(' + angleZ + 'deg)';
-});
+  container.addEventListener("mouseleave", function () {
+    container.style.transform =
+      "rotateX(" +
+      angleY +
+      "deg) rotateY(" +
+      angleX +
+      "deg) rotateZ(" +
+      angleZ +
+      "deg)";
+  });
+}
